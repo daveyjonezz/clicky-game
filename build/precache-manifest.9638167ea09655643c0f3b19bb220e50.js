1f61db5457d8b40dbdeb14f7c1f3de51e5cc7c65
Davis Birsan <davis.birsan@gmail.com>
2019-09-05 22:03:41 -0500

    re-deployed

---
+++ build/precache-manifest.9638167ea09655643c0f3b19bb220e50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c350d9f25cc0b9cf69d88e0cfa7254e4",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "df6e1a129a0a1ee8c8cc",
+    "url": "/clicky-game/static/css/main.205837a6.chunk.css"
+  },
+  {
+    "revision": "a51f7a69db61a516767e",
+    "url": "/clicky-game/static/js/2.9fdd8471.chunk.js"
+  },
+  {
+    "revision": "df6e1a129a0a1ee8c8cc",
+    "url": "/clicky-game/static/js/main.4440acca.chunk.js"
+  },
+  {
+    "revision": "9a76c2d64ea6848a4c8a",
+    "url": "/clicky-game/static/js/runtime~main.cce2267b.js"
+  }
+]);
